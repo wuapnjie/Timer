9c9694e9b28789fe6da41107482eca57347e7cb5
wuapnjie <475384637@qq.com>
2015-10-08 17:33:35 +0800

    要疯啦

--- app/src/main/java/com/iec/dwx/timer/Utils/CacheUtils.java
+++
@@ -1,8 +0,0 @@
-package com.iec.dwx.timer.Utils;
-
-/**
- * Created by Flying SnowBean on 2015/10/2.
- */
-public class CacheUtils {
-    private final String TAG = CacheUtils.class.getSimpleName();
-}
