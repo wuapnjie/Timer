3ccf423931199e742313c3c0a9c18a26d18bdb1a
Eric820320550 <wareric@163.com>
2015-10-10 23:34:32 +0800

    nothing

--- app/src/main/java/com/iec/dwx/timer/Utils/PaletteGetPictureColor.java
+++ app/src/main/java/com/iec/dwx/timer/Utils/PaletteGetPictureColor.java
@@ -1,14 +1,8 @@
 package com.iec.dwx.timer.Utils;
 
-import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.support.v7.graphics.Palette;
-
-import com.iec.dwx.timer.R;
 
 /**
  * Created by Administrator on 2015/10/10 0010.
