a1532e7f1b3ad3627ee6af6f89b23db9bd790264
Eric820320550 <wareric@163.com>
2015-10-23 23:03:40 +0800

    skill finsh

--- app/src/main/java/com/iec/dwx/timer/Views/ViewDragHelperLayout.java
+++ app/src/main/java/com/iec/dwx/timer/Views/ViewDragHelperLayout.java
@@ -6,9 +6,10 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 
 import com.iec.dwx.timer.R;
+import com.iec.dwx.timer.Runnable.SkillUpdatePositionLeft;
+import com.iec.dwx.timer.Runnable.SkillUpdatePositionTop;
 
 /**
  * Created by Administrator on 2015/10/20 0020.
@@ -32,14 +33,18 @@ public class ViewDragHelperLayout extends FrameLayout {
             public int clampViewPositionHorizontal(View child, int left, int dx)
             {
                 System.out.println(child.getTag());
-                child.setTag(R.id.tag_first,left);
+                SkillUpdatePositionLeft runnable=new SkillUpdatePositionLeft(getContext(),(int)child.getTag(R.id.tag_zero),left);
+                Thread td=new Thread(runnable);
+                td.start();
                 return left;
             }
 
             @Override
             public int clampViewPositionVertical(View child, int top, int dy)
             {
-                child.setTag(R.id.tag_second,top);
+                SkillUpdatePositionTop runnable=new SkillUpdatePositionTop(getContext(),(int)child.getTag(R.id.tag_zero),top);
+                Thread td=new Thread(runnable);
+                td.start();
                 return top;
             }
         });
